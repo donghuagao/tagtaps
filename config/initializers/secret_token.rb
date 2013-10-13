c6384ab9a3d83d7815744c40615c9e307f004e78
godavid <donghuagao@aliyun.com>
2013-10-13 22:12:39 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagtaps::Application.config.secret_token = '06191d22fc1968143ebb0ab0faf7fa8c90fcc8974714b2ac5bbbe841bd6f03de11a8a4f94d1c2c9c38b9e714b48ae1c206ba1d698225252b7926af94eaee85c4'
